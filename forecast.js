c8e76e013e6d53d4568cc16edb4b7db6cb3bd839
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-12-09 13:17:09 -0500

    Reformat using semistandard & add badge

--- forecast.js
+++ forecast.js
@@ -3,80 +3,76 @@ var Bluebird = require('bluebird');
 var QueryString = require('querystring');
 var version = require('./package').version;
 
-
-var Forecast = function(options) {
-    if (!options || !options.key) throw new Error('Dark Sky API key must be specified! Get one from "https://darksky.net/dev/".');
-    if (typeof options.key !== 'string') throw new Error('API key must be a string');
-    this.key = options.key;
-    this.timeout = options.timeout || 2000;
+var Forecast = function (options) {
+  if (!options || !options.key) throw new Error('Dark Sky API key must be specified! Get one from "https://darksky.net/dev/".');
+  if (typeof options.key !== 'string') throw new Error('API key must be a string');
+  this.key = options.key;
+  this.timeout = options.timeout || 2000;
 };
 
+Forecast.prototype.fetch = function (latitude, longitude, time, options) {
+  if ((latitude === null || typeof latitude === 'undefined') || (longitude === null || typeof longitude === 'undefined')) throw new Error('Latitude and longitude are required parameters!');
 
-Forecast.prototype.fetch = function(latitude, longitude, time, options) {
+  var path = '/forecast/' + this.key + '/' + latitude + ',' + longitude;
 
-    if ((latitude === null || typeof latitude === 'undefined') || (longitude === null || typeof longitude === 'undefined')) throw new Error('Latitude and longitude are required parameters!');
+  if (time) {
+    var d = new Date(time);
+    if (isNaN(d.getTime())) {
+      options = time;
+    } else {
+      path += ',' + Math.round(d.getTime() / 1000);
+    }
+  }
 
-    var path = '/forecast/' + this.key + '/' + latitude + ',' + longitude;
+  if (typeof (options) === 'object') {
+    path += '?' + QueryString.stringify(options);
+  }
 
-    if (time) {
-        var d = new Date(time);
-        if ( isNaN( d.getTime() ) ) {
-            options = time;
-        } else {
-            path += ',' + Math.round( d.getTime()/1000 ); 
+  var parent = this;
+  var requestOptions = {
+    host: 'api.darksky.net',
+    path: path,
+    method: 'get',
+    headers: { 'User-Agent': 'NodeJS ' + process.version + ' darksky-bluebird (' + version + ')' }
+  };
+
+  return new Bluebird(function (resolve, reject) {
+    var request = Https.request(requestOptions, function (response) {
+      var buffer = '';
+
+      response.setEncoding('utf8');
+
+      response.on('data', function (chunk) {
+        buffer += chunk;
+      });
+
+      response.on('end', function () {
+        try {
+          resolve(JSON.parse(buffer));
+        } catch (error) {
+          reject(new Error('Invalid JSON response from API!'));
         }
-    }
-
-    if (typeof(options) === 'object') {
-        path += '?' + QueryString.stringify(options);
-    }
-
-    var parent = this,
-        requestOptions = {
-            host: 'api.darksky.net',
-            path: path,
-            method: 'get',
-            headers: { 'User-Agent': 'NodeJS ' + process.version + ' darksky-bluebird (' + version + ')' }
-        };
-
-    return new Bluebird(function(resolve, reject) {
-        var request = Https.request(requestOptions, function(response) {
-            var buffer = '';
-
-            response.setEncoding('utf8');
-
-            response.on('data', function(chunk) {
-                buffer += chunk;
-            });
-
-            response.on('end', function() {
-                try {
-                    resolve(JSON.parse(buffer));
-                } catch(error) {
-                    reject(new Error('Invalid JSON response from API!'));
-                }
-            });
-        });
-
-        request.on('error', function(error) {
-            reject(error);
-        });
-
-        request.on('socket', function(socket) {
-            socket.setTimeout(parent.timeout);
-
-            socket.on('timeout', function() {
-                request.abort();
-            });
-
-            socket.on('error', function(error) {
-                reject(error);
-            });
-        });
-
-        request.end();
+      });
     });
-};
 
+    request.on('error', function (error) {
+      reject(error);
+    });
+
+    request.on('socket', function (socket) {
+      socket.setTimeout(parent.timeout);
+
+      socket.on('timeout', function () {
+        request.abort();
+      });
+
+      socket.on('error', function (error) {
+        reject(error);
+      });
+    });
+
+    request.end();
+  });
+};
 
 module.exports = Forecast;
